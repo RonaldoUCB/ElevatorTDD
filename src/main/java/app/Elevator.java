79f64faba558703512e4eb9992f1f8d13b35dc2f
RONALDO ROJAS AYALA <ronaldorojasayala@192.168.0.101>
2022-10-21 02:43:03 -0400

    green bar for elevator starting floor

--- src/main/java/app/Elevator.java
+++ src/main/java/app/Elevator.java
@@ -1,8 +1,20 @@
 package app;
 
 public class Elevator {
+    // properties
+    private int elevatorCurrentFloor;
+
+    // constructors
+    public Elevator() {
+    }
+
+    // methods
     public Person createPerson() {
         Person person = new Person();
         return person;
+    }
+
+    public int getElevatorCurrentFloor() {
+        return 1;
     }
 }
